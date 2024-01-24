3b251f49ea8a16564dc66234ae611d2b64e6f861
Documenter.jl <documenter@juliadocs.github.io>
2024-01-24 20:20:59 +0000

    build based on a3955b1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Endatabas","category":"page"},{"location":"#Endatabas","page":"Home","title":"Endatabas","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Endatabas.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Endatabas]","category":"page"}]
+}
